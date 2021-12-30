5e14ade252d9081121d99b943e68c689ba4b8679
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2021-12-30 20:16:09 +0200

    graphicsManager thread

--- src/graphicsEngine/GraphicsManager.java
+++ src/graphicsEngine/GraphicsManager.java
@@ -7,7 +7,7 @@ import graphicsEngine.utilities.input.InputManager;
 
 import java.util.ArrayList;
 
-public class GraphicsManager {
+public class GraphicsManager implements Runnable{
 
     public static GraphicsData data;
     public static PageManager pages;
@@ -16,12 +16,17 @@ public class GraphicsManager {
     public static InputManager input;
     public static ExitManager exitManager;
 
+    private static final long refreshDelay = 30; //screen refresh delay in millis
     public static boolean running;
 
-    public static void initialize(ArrayList<Page> _pages){
-        System.out.println("GraphicsManager: Initializing.");
-
+    public GraphicsManager(){
+        System.out.println("Graphics Engine: Creating.");
         data = new GraphicsData(true, true);
+    }
+
+    public static void initialize(ArrayList<Page> _pages){
+        System.out.println("Graphics Engine: Initializing.");
+
         pages = new PageManager(_pages);
 
         graphics = new GraphicsClass();
@@ -36,11 +41,21 @@ public class GraphicsManager {
         running = true;
     }
 
-    public static void run(){
-        if (!data.windowParameters.minimized) {
-            graphics.main();
-            Page activePage = pages.pages.get(pages.activePage);
-            activePage.inputCheck.check(input, activePage);
+    @SuppressWarnings("BusyWait")
+    @Override
+    public void run(){
+        while(running){
+            if (!data.windowParameters.minimized) {
+                graphics.main();
+                Page activePage = pages.pages.get(pages.activePage);
+                activePage.inputCheck.check(input, activePage);
+            }
+
+            try {
+                Thread.sleep(refreshDelay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/graphicsEngine/demo/simple/DemoSimple.java
+++ src/graphicsEngine/demo/simple/DemoSimple.java
@@ -7,33 +7,21 @@ import java.util.ArrayList;
 
 public class DemoSimple {
 
-    private static final long refreshDelay = 20; //screen refresh delay in millis
-
     public static void main(String[] args){
         System.out.println("Simple graphics demo started.");
-
-        setup();
-        run();
+        start();
     }
 
-    private static void setup(){
+    private static void start(){
+        GraphicsManager graphics = new GraphicsManager();
+
         GraphicsManager.initialize(
                 new ArrayList<>(){{
                     add(new Page_0());
                     add(new Page_1());
                 }}
         );
-    }
 
-    private static void run(){
-        while (GraphicsManager.running){
-            GraphicsManager.run();
-
-            try {
-                Thread.sleep(refreshDelay);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        graphics.run();
     }
 }
