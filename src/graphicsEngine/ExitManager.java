2f31e7dd3df97ac02956941baacf1d73a9e70545
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-01-11 00:43:15 +0200

    todo

--- src/graphicsEngine/ExitManager.java
+++ src/graphicsEngine/ExitManager.java
@@ -1,13 +1,16 @@
 package graphicsEngine;
 
+// TODO: add javadoc
 public class ExitManager {
     private static final String EXIT_MESSAGE = ": Closing.";
     private final String graphicsEngineName;
 
+    // TODO: add javadoc
     ExitManager(String graphicsEngineName) {
         this.graphicsEngineName = graphicsEngineName;
     }
 
+    // TODO: add javadoc
     public void close() {
         System.out.println(graphicsEngineName + EXIT_MESSAGE);
         GraphicsManager.graphics.window.dispose();
