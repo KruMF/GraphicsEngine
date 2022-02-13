8272c2a39c78db4e78664803d68ff55788722440
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-02-14 00:31:11 +0200

    table

--- src/main/java/graphicsEngine/demo/simple/pages/page2/Center.java
+++ src/main/java/graphicsEngine/demo/simple/pages/page2/Center.java
@@ -3,12 +3,19 @@ package graphicsEngine.demo.simple.pages.page2;
 import graphicsEngine.utilities.GraphicsHelper;
 import graphicsEngine.utilities.simpleParts.SimplePart;
 
+import graphicsEngine.demo.simple.pages.page2.grid.Table;
+
 import java.awt.*;
 
 import org.jetbrains.annotations.NotNull;
 
 //TODO: add javadoc
 class Center extends SimplePart {
+    private static final boolean DRAW_PAGE_MESSAGE = true;
+    private static final int TABLE_MARGIN = 30;
+
+    private static final Table table = new Table();
+
     //TODO: add javadoc
     protected Center() {
         super();
@@ -19,6 +26,26 @@ class Center extends SimplePart {
     public void draw(@NotNull Graphics g, int[] location, int[] size) {
         super.draw(g, location, size);
 
+        table.draw(g, getTableLocation(), getTableSize());
+
+        if (DRAW_PAGE_MESSAGE) {
+            drawPageMessage(g);
+        }
+    }
+
+    private int[] getTableLocation() {
+        return new int[] {
+                location[0] + Math.min(size[0], TABLE_MARGIN),
+                location[1] + Math.min(size[1], TABLE_MARGIN)};
+    }
+
+    private int[] getTableSize() {
+        return new int[] {
+                Math.max(0, size[0] - 2 * TABLE_MARGIN),
+                Math.max(0, size[1] - 2 * TABLE_MARGIN)};
+    }
+
+    private void drawPageMessage(Graphics g) {
         g.setColor(Color.green);
         GraphicsHelper.drawStrings_VA(
                 g, 0,
@@ -26,7 +53,5 @@ class Center extends SimplePart {
                 new int[] {
                         location[0] + size[0] / 2,
                         location[1] + size[1] / 2});
-
-        //draw stuff here
     }
 }
---
+++ src/main/java/graphicsEngine/demo/simple/pages/page2/grid/Table.java
@@ -0,0 +1,105 @@
+package graphicsEngine.demo.simple.pages.page2.grid;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Table {
+    private static final int
+            COLUMN_COUNT = 5,
+            ROW_COUNT = 5;
+    private static final Color GRID_COLOR = Color.white;
+
+    private ArrayList<ArrayList<Cell>> table;
+
+    public Table() {
+        table = defaultTable();
+    }
+
+    private static ArrayList<ArrayList<Cell>> defaultTable() {
+        return new ArrayList<>() {{
+            for (int i = 0; i < COLUMN_COUNT; i++) {
+                add(new ArrayList<>() {{
+                    for (int j = 0; j < ROW_COUNT; j++) {
+                        add(new Cell());
+                    }
+                }});
+            }
+        }};
+    }
+
+    // TODO:
+    //      * finish this
+    //      * call this to update values
+    //      * add javadoc
+    public void update() {
+        //check every column
+        for (int i = 0; i < table.size(); i++) {
+            ArrayList<Cell> column = table.get(i);
+
+            //check every row
+            for (int j = 0; j < column.size(); j++) {
+                Cell cell = column.get(j);
+
+                // update cell
+            }
+        }
+    }
+
+    public void draw(Graphics g, int[] location, int[] size) {
+        drawCells(g, location, size);
+        drawGrid(g, location, size);
+    }
+
+    private void drawCells(Graphics g, int[] location, int[] size) {
+        double[] cell_size = new double[] {
+                (double) size[0] / COLUMN_COUNT,
+                (double) size[1] / ROW_COUNT};
+
+        //check every column
+        for (int i = 0; i < table.size(); i++) {
+            ArrayList<Cell> column = table.get(i);
+
+            //check every row
+            for (int j = 0; j < column.size(); j++) {
+                Cell cell = column.get(j);
+
+                cell.draw(g,
+                        new int[] {
+                                location[0] + (int) (i * cell_size[0]),
+                                location[1] + (int) (j * cell_size[1])},
+                        new int[] {
+                                (int) cell_size[0],
+                                (int) cell_size[1]});
+            }
+        }
+    }
+
+    private void drawGrid(Graphics g, int[] location, int[] size) {
+        g.setColor(GRID_COLOR);
+
+        drawHorizontalLines(g, location, size);
+        drawVerticalLines(g, location, size);
+    }
+
+    private void drawVerticalLines(Graphics g, int[] location, int[] size) {
+        double column_width = (double) size[0] / COLUMN_COUNT;
+        for (int i = 0; i <= COLUMN_COUNT; i++) {
+            int x = location[0] + (int) (i * column_width);
+
+            g.drawLine(
+                    x, location[1],
+                    x, location[1] + size[1]);
+        }
+    }
+
+    private void drawHorizontalLines(Graphics g, int[] location, int[] size) {
+        double row_height = (double) size[1] / ROW_COUNT;
+        for (int i = 0; i <= ROW_COUNT; i++) {
+            int y = location[1] + (int) (i * row_height);
+
+            g.drawLine(
+                    location[0], y,
+                    location[0] + size[0], y);
+        }
+    }
+}
