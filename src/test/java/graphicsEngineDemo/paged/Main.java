863d396350beedcadc7955bf5e8a231607b47c93
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-10 02:50:14 +0300

    new paged demo stub

---
+++ src/test/java/graphicsEngineDemo/paged/Main.java
@@ -0,0 +1,9 @@
+package graphicsEngineDemo.paged;
+
+import static graphicsEngineSwingBeta.Utilities.printLine;
+
+public class Main {
+    public static void main(String[] args) {
+        printLine("Paged demo currently not implemented");
+    }
+}
