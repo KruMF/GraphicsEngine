cf7cdc9c2abc4cec764bc82ac05ee2aabef8907e
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-09 22:26:57 +0300

    starting work on 0.8.0

---
+++ src/main/java/graphicsEngineSwingBeta/Utilities.java
@@ -0,0 +1,16 @@
+package graphicsEngineSwingBeta;
+
+/**
+ * A utility class for containing generic methods.
+ */
+public class Utilities {
+
+    /**
+     * Prints a message in console as new line.
+     *
+     * @param message Message to print.
+     */
+    static void printLine(String message) {
+        System.out.println(message);
+    }
+}
