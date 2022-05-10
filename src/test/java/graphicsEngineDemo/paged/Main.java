dbf2b3b368cfcad519fd0e87d921dfc5ecee9933
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-10 03:06:33 +0300

    todo

--- src/test/java/graphicsEngineDemo/paged/Main.java
+++ src/test/java/graphicsEngineDemo/paged/Main.java
@@ -2,6 +2,7 @@ package graphicsEngineDemo.paged;
 
 import static graphicsEngineSwingBeta.Utilities.printLine;
 
+//TODO: finish this
 public class Main {
     public static void main(String[] args) {
         printLine("Paged demo currently not available");
