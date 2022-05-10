4015b9791e2fade297c1b1214638fddf96f98cc0
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-10 03:06:17 +0300

    standalone rotor calculator stub

---
+++ src/test/java/rotorCalculator/main/Main.java
@@ -0,0 +1,10 @@
+package rotorCalculator.main;
+
+import static graphicsEngineSwingBeta.Utilities.printLine;
+
+//TODO: finish this
+public class Main {
+    public static void main(String[] args) {
+        printLine("Rotor calculator currently not available");
+    }
+}
