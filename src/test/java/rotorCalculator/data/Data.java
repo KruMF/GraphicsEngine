6d350a42ad541011841676050681d7e27f7e999b
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-13 14:03:52 +0300

    cleanup

--- src/test/java/rotorCalculator/data/Data.java
+++ src/test/java/rotorCalculator/data/Data.java
@@ -32,25 +32,4 @@ public class Data {
     public static double getGravity() {
         return generalConstants.standardGravity * preferences.gForce;
     }
-
-    public static final int MAX_X = 200;
-    private static int x;
-
-    //add more parameters here
-
-    public static void resetValues() {
-        resetX();
-    }
-
-    public static void resetX() {
-        x = 0;
-    }
-
-    public static int getX() {
-        return x;
-    }
-
-    public static void incrementX() {
-        x++;
-    }
 }
