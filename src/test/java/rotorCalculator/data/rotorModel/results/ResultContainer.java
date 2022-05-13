f229a329de3e0e2e08888ca68e970844900961c3
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-05-13 03:49:14 +0300

    result container improvement

--- src/test/java/rotorCalculator/data/rotorModel/results/ResultContainer.java
+++ src/test/java/rotorCalculator/data/rotorModel/results/ResultContainer.java
@@ -1,11 +1,31 @@
 package rotorCalculator.data.rotorModel.results;
 
+//TODO: finish this and add javadoc
 public class ResultContainer {
-    ParticularResults.JointLimitResults jointLimitResults;
-    ParticularResults.CoriolisLimitResults coriolisLimitResults;
-    ParticularResults.TotalOmegaLimitResults totalOmegaLimitResults;
-    ParticularResults.GravityRequirementResults gravityRequirementResults;
-    ParticularResults.GradientLimitResults gradientLimitResults;
-    ParticularResults.TotalRadiusLimitResults totalRadiusLimitResults;
-    ParticularResults.ActualParameterResults actualParameterResults;
+    public ParticularResults.JointLimitResults jointLimitResults;
+    public ParticularResults.CoriolisLimitResults coriolisLimitResults;
+    public ParticularResults.TotalOmegaLimitResults totalOmegaLimitResults;
+    public ParticularResults.GravityRequirementResults gravityRequirementResults;
+    public ParticularResults.GradientLimitResults gradientLimitResults;
+    public ParticularResults.TotalRadiusLimitResults totalRadiusLimitResults;
+    public ParticularResults.ActualParameterResults actualParameterResults;
+
+    public ResultContainer() {
+        initializeResults();
+        getResults();
+    }
+
+    private void initializeResults() {
+        jointLimitResults = new ParticularResults.JointLimitResults();
+        coriolisLimitResults = new ParticularResults.CoriolisLimitResults();
+        totalOmegaLimitResults = new ParticularResults.TotalOmegaLimitResults();
+        gravityRequirementResults = new ParticularResults.GravityRequirementResults();
+        gradientLimitResults = new ParticularResults.GradientLimitResults();
+        totalRadiusLimitResults = new ParticularResults.TotalRadiusLimitResults();
+        actualParameterResults = new ParticularResults.ActualParameterResults();
+    }
+
+    public void getResults() {
+        //TODO: get actual results here
+    }
 }
