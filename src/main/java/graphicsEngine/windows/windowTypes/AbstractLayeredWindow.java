97a90176843242e9fcdba9c7f7f0656e7e9b7cb5
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-09-07 03:31:51 +0300

    overlay improvements

--- src/main/java/graphicsEngine/windows/windowTypes/AbstractLayeredWindow.java
+++ src/main/java/graphicsEngine/windows/windowTypes/AbstractLayeredWindow.java
@@ -4,6 +4,7 @@ import graphicsEngine.windows.AbstractWindow;
 import graphicsEngine.windows.WindowConfig;
 import graphicsEngine.presets.SimpleOverlay;
 
+import java.awt.Component;
 import java.util.Objects;
 
 import org.jetbrains.annotations.NotNull;
@@ -12,28 +13,44 @@ import org.jetbrains.annotations.Nullable;
 //contents and overlays
 //TODO: finish this and add javadocs
 abstract class AbstractLayeredWindow extends AbstractWindow {
-    private static final SimpleOverlay DEFAULT_OVERLAY = new SimpleOverlay(null);
 
-    private SimpleOverlay overlay;
-
+    //TODO: add javadoc
     AbstractLayeredWindow(@NotNull WindowConfig config, @Nullable SimpleOverlay overlay) {
         super(config);
         setOverlay(overlay);
         hideOverlay();
     }
 
+    //TODO: add javadoc
     void setOverlay(@Nullable SimpleOverlay overlay) {
-        setGlassPane(Objects.requireNonNullElse(overlay, DEFAULT_OVERLAY));
+        setGlassPane(Objects.requireNonNullElse(overlay, getDefaultOverlay()));
     }
 
+    private static SimpleOverlay getDefaultOverlay() {
+        return new SimpleOverlay(null);
+    }
+
+    //TODO: finish this and add javadoc
+    public @Nullable SimpleOverlay getOverlay() {
+        Component glassPane = getGlassPane();
+        if (glassPane instanceof SimpleOverlay) {
+            return (SimpleOverlay) glassPane;
+        } else {
+            return null;
+        }
+    }
+
+    //TODO: add javadoc
     public void showOverlay() {
         getGlassPane().setVisible(true);
     }
 
+    //TODO: add javadoc
     public void hideOverlay() {
         getGlassPane().setVisible(false);
     }
 
+    //TODO: add javadoc
     public void toggleOverlay() {
         getGlassPane().setVisible(!getGlassPane().isVisible());
     }
