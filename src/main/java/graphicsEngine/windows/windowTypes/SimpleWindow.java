c4510ace3c0f6c23cd6ffb006b0dc0ebb80b9d6d
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-09-08 19:12:28 +0300

    docs

--- src/main/java/graphicsEngine/windows/windowTypes/SimpleWindow.java
+++ src/main/java/graphicsEngine/windows/windowTypes/SimpleWindow.java
@@ -5,9 +5,15 @@ import graphicsEngine.windows.WindowConfig;
 
 import org.jetbrains.annotations.NotNull;
 
-//has only contents, nothing much to add
-//TODO: finish this and add javadocs
+/**
+ * A simple window; has only contents.
+ */
 public abstract class SimpleWindow extends AbstractWindow {
+    /**
+     * Creates a new SimpleWindow with specified configuration.
+     *
+     * @param config Window configuration.
+     */
     public SimpleWindow(@NotNull WindowConfig config) {
         super(config);
     }
